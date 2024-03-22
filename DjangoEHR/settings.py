52c730175f7c8449ef6ced6e9ff6377de0bfdf49
ghada-elboghdady <115363720+ghada-elboghdady@users.noreply.github.com>
2024-03-22 18:06:51 +0200

    databasepostgres

--- DjangoEHR/settings.py
+++ DjangoEHR/settings.py
@@ -77,26 +77,26 @@ WSGI_APPLICATION = 'DjangoEHR.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# version 1
-DATABASES = {
-   'default': {
-      'ENGINE': 'django.db.backends.sqlite3',
-      'NAME': BASE_DIR / 'db.sqlite3',
-   }
-  }
-
-# # version2
+# # version 1
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'lab3database',
-#         'PASSWORD': 'Mypassword3',
-#         'HOST': 'ehrserver.postgres.database.azure.com',
-#         'PORT': '5432',
-#         'OPTIONS':{'sslmode':'require'},
-#     }
-# }
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#   }
+
+# version2
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'ghada',
+        'PASSWORD': 'g&838383',
+        'HOST': 'ghada4220132.postgres.database.azure.com',
+        'PORT': '5432',
+        'OPTIONS':{'sslmode':'require'},
+    }
+}
 
 
 # Password validation
